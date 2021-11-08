e36aa4dec100ac7b176e4a757d94bebb504f79e8
Krzysiek <szlauerdev@gmail.com>
2021-11-08 09:16:06 +0100

    Add new empty database

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ Bootstrap(app)
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 # second argument is used if a file is run locally
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL2", "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
@@ -94,7 +94,7 @@ class Comment(db.Model):
     comment_post = relationship("BlogPost", back_populates="posts")
     post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"))
 
-# db.create_all()
+db.create_all()
 
 gravatar = Gravatar(app,
                     size=200,
